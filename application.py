73ef282b14feb254e0618501817e5d817749b074
Cyndi Lopez <cyndilopez6@gmail.com>
2019-07-29 14:03:30 -0700

    predict route, which calls external api client, also now loading machine learning model and predicting what image says (currently it's pulling an image from local machine

--- application.py
+++ application.py
@@ -1,15 +1,16 @@
 from flask import Flask
 import flask
-
 from PIL import Image
 import cv2
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import copy
-from helpers import clean_data_results, verifySize, return_data_openalpr, get_coord
+from helpers import *
 import boto3 # a low-level client representing S3
 from config import S3_BUCKET, S3_KEY, S3_SECRET
 import tempfile
+import numpy as np
+from keras.preprocessing import image
+from zeep import Client
 
 application = Flask(__name__)
 
@@ -54,7 +55,7 @@ def detect():
     
     # print("temp ", tmp.name)
 
-    # IMAGE_PATH = '/Users/cylopez/Documents/projects/license-plate-recognition/us/us3.jpg'
+    IMAGE_PATH = '/Users/cylopez/Documents/projects/license-plate-recognition/us/us3.jpg'
 
     # data_des = return_data_openalpr(tmp.name)
     # data_des = return_data_openalpr(IMAGE_PATH)
@@ -63,79 +64,76 @@ def detect():
         'state': 'il', 
         'plate': '9185914'
         }
-    # # min_xcoord, min_ycoord, max_xcoord, max_ycoord = get_coord(data_des)   
-    # min_xcoord, min_ycoord, max_xcoord, max_ycoord = 207, 209, 258, 296
-    # img = cv2.imread(IMAGE_PATH)
-    # img_crop = img[int(min_ycoord):int(max_ycoord),int(min_xcoord):int(max_xcoord)]
-    # height_img_crop = max_ycoord - min_ycoord
+    # min_xcoord, min_ycoord, max_xcoord, max_ycoord = get_coord(data_des)   
+    min_xcoord, min_ycoord, max_xcoord, max_ycoord = 207, 209, 258, 296
+    img = cv2.imread(IMAGE_PATH)
+    img_crop = img[int(min_ycoord):int(max_ycoord),int(min_xcoord):int(max_xcoord)]
+    height_img_crop = max_ycoord - min_ycoord
     
-    # # image processing colors
-    # img_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
-    # ddepth = cv2.CV_8U
-    # ret, th = cv2.threshold(img_gray,80,255,cv2.THRESH_BINARY_INV)
+    # image processing colors
+    img_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
+    ddepth = cv2.CV_8U
+    ret, th = cv2.threshold(img_gray,80,255,cv2.THRESH_BINARY_INV)
     
-    # # find contours in image
-    # img_contours = copy.copy(th)
-    # contours, hierarchy = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    # contours_after_size_verification = []
-    # for contour in contours:
-    #     if verifySize(th, contour, height_img_crop):
-    #         contours_after_size_verification.append(contour)
+    # find contours in image
+    img_contours = copy.copy(th)
+    contours, hierarchy = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours_after_size_verification = []
+    for contour in contours:
+        if verifySize(th, contour, height_img_crop):
+            contours_after_size_verification.append(contour)
+    contour_num = 0
+    x_locations = []
+    pred_chars = {}
+    for contour in contours_after_size_verification:
+            x, y, w, h = cv2.boundingRect(contour)
+            paddingw = int(w/3)
+            paddingh = int(h/4)
+            img_crop = cv2.getRectSubPix(img_contours, (w+paddingw,h+paddingh), (x+w/2,y+h/2))
+            x_locations.append(x)
+            resized_image = cv2.resize(img_crop,(224,224))
+            cv2.imwrite("contour" + str(contour_num) + ".png",resized_image)
+            img = cv2.imread("contour" + str(contour_num) +  ".png")
+            img = image.img_to_array(img)
+            img = np.expand_dims(img, axis = 0)
+            print(img.shape)
 
-    # for contour in contours_after_size_verification:
-    #         x, y, w, h = cv2.boundingRect(contour)
-    #         paddingw = int(w/5)
-    #         paddingh = int(h/5)
-    #         img_crop = cv2.getRectSubPix(img_contours, (w+paddingw,h+paddingh), (x+w/2,y+h/2))
-    #         resized_image = cv2.resize(img_crop,(34,85))
-    #         # feed in image to machine learning model
-    
+            # feed in image to machine learning model
+            model = load_model_p()
+            model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+            #predict the result
+            result = model.predict(img)
+            predictions = np.argmax(result, axis=1) #in an array
+            character = list(CATEGORIES.keys())[list(CATEGORIES.values()).index(predictions[0])]
+            print(character)      
+            pred_chars[character] = x  
+            
+    print(pred_chars)
+    sorted_chars = sorted(pred_chars.items(), key=lambda kv: kv[1])
+    pred_plate = [j[0] for j in sorted_chars]
+    pred_plate = ''.join(pred_plate)
     data = {"plate": data_des["plate"],
-            "state": data_des["state"]}
+            "state": data_des["state"],
+            "predicted": pred_plate}
 
     return flask.jsonify(data)
 
-@application.route("/predict", methods=["POST"])
+# @application.route("/predict", methods=["POST"])
+@application.route("/predict")
+
 def predict():
-    print("Predicting")
-    # initialize the data dictionary that will be returned from the
-    # view
-    data = {"success": False}
+    # need to provide license plate from front end
+    data = {'username': 'cyft369',
+        'RegistrationNumber': 'YY07XHH'}
 
-    # ensure an image was properly uploaded to our endpoint
-    data["success"] = True
-    print(flask.request.data.image)
-    if flask.request.files.get("image"):
-        print("got image")
-    elif flask.request.form["image"]:
-        print("image in second form")
-    #     # read the image in PIL format
-    #     image = flask.request.files["image"].read()
-    #     print(image)
-    #     print("read image")
-    #     image = Image.open(io.BytesIO(image))
-    #     print(image)
-    #     print("opened image")
-    else:
-        print("didnt get image")
-    #     # preprocess the image and prepare it for classification
-    print("after if statement")
-    # #     # # classify the input image and then initialize the list
-    # #     # # of predictions to return to the client
-    # #     # preds = model.predict(image)
-    # #     # results = imagenet_utils.decode_predictions(preds)
-    # #     # data["predictions"] = []
-
-    # #     # # loop over the results and add them to the list of
-    # #     # # returned predictions
-    # #     # for (imagenetID, label, prob) in results[0]:
-    # #     #     r = {"label": label, "probability": float(prob)}
-    # #     #     data["predictions"].append(r)
-
-    # #     # indicate that the request was a success
-
-    # # return the data dictionary as a JSON response
-    return flask.jsonify(data)
+    # client = Client("http://www.regcheck.org.uk/api/reg.asmx?WSDL")
+    # result = client.service.Check(data['RegistrationNumber'], data['username'])
+    # vehicleJson = result.vehicleJson
+    # cleaned_data = clean_vehicle_info_data(vehicleJson)
+    
+    # return flask.jsonify(cleaned_data)
+    return flask.jsonify(clean_vehicle_info_data(fake_clean_data()))
 
 if __name__ == '__main__':
     application.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -1,9 +1,16 @@
 import cv2
 import requests
 import json
-import matplotlib.pyplot as plt
 import base64
 from config import OPENALPR_SECRET_KEY
+from keras.models import load_model
+
+CATEGORIES = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, 
+                'A': 9, 'B': 10, 'C': 11, 'D': 12, 'E': 13, 'F': 14, 'G': 15, 'H': 16, 
+                'I': 17, 'J': 18, 'K': 19, 'L': 20, 'M': 21, 'N': 22, 'O': 23, 'P': 24, 
+                'R': 25, 'S': 26, 'T': 27, 'U': 28, 'V': 29, 'W': 30, 'X': 31, 'Y': 32, 
+                'Z': 33}
+
 def clean_data_results(data):
         des_data = {
             'coordinates': data["coordinates"],
@@ -19,7 +26,7 @@ def verifySize(th, contour, img_height):
     min_char_to_plate_aspect = 40/224
     aspect = 2.5/(2.5*2.5) #based on eyeballing
     charAspect = w/h
-    error = 0.4
+    error = 0.6
     minHeight = 30
     minAspect = 0.2
     maxAspect = aspect+aspect*error
@@ -50,322 +57,53 @@ def get_coord(data_des):
         max_ycoord = max(pair["y"] for pair in data_des["coordinates"])
         return min_xcoord, min_ycoord, max_xcoord, max_ycoord
 
-# {
-#   "uuid": "",
-#   "data_type": "alpr_results",
-#   "epoch_time": 1563554171947,
-#   "processing_time": {
-#     "total": 576.9929999951273,
-#     "plates": 94.29983520507812,
-#     "vehicles": 480.1889999944251
-#   },
-#   "img_height": 375,
-#   "img_width": 500,
-#   "results": [
-#     {
-#       "plate": "9185914",
-#       "confidence": 93.7087631225586,
-#       "region_confidence": 99,
-#       "vehicle_region": {
-#         "y": 0,
-#         "x": 64,
-#         "height": 375,
-#         "width": 375
-#       },
-#       "region": "il",
-#       "plate_index": 0,
-#       "processing_time_ms": 36.85621643066406,
-#       "candidates": [
-#         {
-#           "matches_template": 1,
-#           "plate": "9185914",
-#           "confidence": 93.7087631225586
-#         },
-#         {
-#           "matches_template": 1,
-#           "plate": "918591",
-#           "confidence": 80.28701782226562
-#         }
-#       ],
-#       "coordinates": [
-#         {
-#           "y": 209,
-#           "x": 207
-#         },
-#         {
-#           "y": 212,
-#           "x": 296
-#         },
-#         {
-#           "y": 258,
-#           "x": 296
-#         },
-#         {
-#           "y": 255,
-#           "x": 207
-#         }
-#       ],
-#       "vehicle": {
-#         "orientation": [
-#           {
-#             "confidence": 99.11315155029297,
-#             "name": "180"
-#           },
-#           {
-#             "confidence": 0.8563453555107117,
-#             "name": "225"
-#           },
-#           {
-#             "confidence": 0.03050280176103115,
-#             "name": "135"
-#           },
-#           {
-#             "confidence": 2.5378501504746964e-06,
-#             "name": "0"
-#           },
-#           {
-#             "confidence": 6.952996045583859e-08,
-#             "name": "45"
-#           },
-#           {
-#             "confidence": 2.7187482487533998e-08,
-#             "name": "missing"
-#           },
-#           {
-#             "confidence": 1.9718989108241658e-08,
-#             "name": "270"
-#           },
-#           {
-#             "confidence": 9.061150052502853e-09,
-#             "name": "315"
-#           },
-#           {
-#             "confidence": 9.841935044718753e-10,
-#             "name": "90"
-#           }
-#         ],
-#         "color": [
-#           {
-#             "confidence": 99.07832336425781,
-#             "name": "white"
-#           },
-#           {
-#             "confidence": 0.8844082951545715,
-#             "name": "silver-gray"
-#           },
-#           {
-#             "confidence": 0.030218694359064102,
-#             "name": "gold-beige"
-#           },
-#           {
-#             "confidence": 0.00497695105150342,
-#             "name": "blue"
-#           },
-#           {
-#             "confidence": 0.0012463860912248492,
-#             "name": "black"
-#           },
-#           {
-#             "confidence": 0.0005867808358743787,
-#             "name": "yellow"
-#           },
-#           {
-#             "confidence": 0.00012517017603386194,
-#             "name": "green"
-#           },
-#           {
-#             "confidence": 7.873159484006464e-05,
-#             "name": "red"
-#           },
-#           {
-#             "confidence": 3.4619952202774584e-05,
-#             "name": "orange"
-#           },
-#           {
-#             "confidence": 4.2736951400002e-06,
-#             "name": "brown"
-#           }
-#         ],
-#         "make": [
-#           {
-#             "confidence": 99.99864959716797,
-#             "name": "ford"
-#           },
-#           {
-#             "confidence": 0.0007877767784520984,
-#             "name": "mercury"
-#           },
-#           {
-#             "confidence": 0.0004813948180526495,
-#             "name": "chevrolet"
-#           },
-#           {
-#             "confidence": 5.5553202400915325e-05,
-#             "name": "cadillac"
-#           },
-#           {
-#             "confidence": 6.3391953517566435e-06,
-#             "name": "shelby"
-#           },
-#           {
-#             "confidence": 2.6975640139426105e-06,
-#             "name": "buick"
-#           },
-#           {
-#             "confidence": 2.258077529404545e-06,
-#             "name": "mazda"
-#           },
-#           {
-#             "confidence": 1.3764256436843425e-06,
-#             "name": "aston-martin"
-#           },
-#           {
-#             "confidence": 9.72486418504559e-07,
-#             "name": "dodge"
-#           },
-#           {
-#             "confidence": 9.36862875278166e-07,
-#             "name": "scion"
-#           }
-#         ],
-#         "body_type": [
-#           {
-#             "confidence": 99.99334716796875,
-#             "name": "sedan-sports"
-#           },
-#           {
-#             "confidence": 0.00621750857681036,
-#             "name": "sedan-standard"
-#           },
-#           {
-#             "confidence": 0.0003934820997528732,
-#             "name": "motorcycle"
-#           },
-#           {
-#             "confidence": 1.8562377590569668e-05,
-#             "name": "truck-standard"
-#           },
-#           {
-#             "confidence": 8.484757927362807e-06,
-#             "name": "sedan-convertible"
-#           },
-#           {
-#             "confidence": 1.520516661912552e-06,
-#             "name": "sedan-wagon"
-#           },
-#           {
-#             "confidence": 4.905758714812691e-07,
-#             "name": "suv-crossover"
-#           },
-#           {
-#             "confidence": 2.757722405988261e-08,
-#             "name": "suv-standard"
-#           },
-#           {
-#             "confidence": 1.8569892290543066e-08,
-#             "name": "tractor-trailer"
-#           },
-#           {
-#             "confidence": 5.603315589297608e-09,
-#             "name": "antique"
-#           }
-#         ],
-#         "year": [
-#           {
-#             "confidence": 88.11061096191406,
-#             "name": "2005-2009"
-#           },
-#           {
-#             "confidence": 10.45720386505127,
-#             "name": "2010-2014"
-#           },
-#           {
-#             "confidence": 1.4172221422195435,
-#             "name": "2000-2004"
-#           },
-#           {
-#             "confidence": 0.013416082598268986,
-#             "name": "1995-1999"
-#           },
-#           {
-#             "confidence": 0.0014636542182415724,
-#             "name": "1990-1994"
-#           },
-#           {
-#             "confidence": 7.90458798292093e-05,
-#             "name": "1985-1989"
-#           },
-#           {
-#             "confidence": 3.0213254831323866e-06,
-#             "name": "1980-1984"
-#           },
-#           {
-#             "confidence": 1.779733565854258e-06,
-#             "name": "2015-2019"
-#           },
-#           {
-#             "confidence": 2.5024817773555696e-08,
-#             "name": "missing"
-#           }
-#         ],
-#         "make_model": [
-#           {
-#             "confidence": 99.29950714111328,
-#             "name": "ford_mustang"
-#           },
-#           {
-#             "confidence": 0.6989838480949402,
-#             "name": "ford_gt"
-#           },
-#           {
-#             "confidence": 0.0009800600819289684,
-#             "name": "chevrolet_camaro"
-#           },
-#           {
-#             "confidence": 0.00012154193973401561,
-#             "name": "mercury_grand-marquis"
-#           },
-#           {
-#             "confidence": 7.879921031417325e-05,
-#             "name": "cadillac_sts"
-#           },
-#           {
-#             "confidence": 7.13369736331515e-05,
-#             "name": "toyota_celica"
-#           },
-#           {
-#             "confidence": 6.624715751968324e-05,
-#             "name": "ford_f-150"
-#           },
-#           {
-#             "confidence": 5.879783202544786e-05,
-#             "name": "ford_flex"
-#           },
-#           {
-#             "confidence": 4.6700752136530355e-05,
-#             "name": "ford_ranger"
-#           },
-#           {
-#             "confidence": 3.954941712436266e-05,
-#             "name": "cadillac_seville"
-#           }
-#         ]
-#       },
-#       "matches_template": 1,
-#       "requested_topn": 10
-#     }
-#   ],
-#   "credits_monthly_used": 3,
-#   "version": 2,
-#   "credits_monthly_total": 1000,
-#   "error": False,
-#   "regions_of_interest": [
-#     {
-#       "y": 0,
-#       "x": 0,
-#       "height": 375,
-#       "width": 500
-#     }
-#   ],
-#   "credit_cost": 1
-# }
+def load_model_p():
+        model = load_model('resnet50-weights.04-1.35.hdf5')
+        return model
+
+def clean_vehicle_info_data(data):
+        cleaned_data = {
+                "description":data["Description"],
+                "registration_year":data["RegistrationYear"],
+                "car_make":data["CarMake"]["CurrentTextValue"],
+                "car_model":data["CarModel"]["CurrentTextValue"],
+                "engine_size":data["EngineSize"]["CurrentTextValue"],
+                "body_style":data["BodyStyle"]["CurrentTextValue"],
+                "fuel_type":data["FuelType"]["CurrentTextValue"],
+                "color":data["Colour"],
+                "vehicle_identification_number":data["VehicleIdentificationNumber"],
+                "engine_number":data["EngineNumber"]
+        }
+        return cleaned_data
+def fake_clean_data():
+        return {
+                "Description": "PEUGEOT 307 X-LINE",
+                "RegistrationYear": "2007",
+                "CarMake": {
+                "CurrentTextValue": "PEUGEOT"
+                },
+                "CarModel": {
+                "CurrentTextValue": "307 X-LINE"
+                },
+                "MakeDescription": {
+                "CurrentTextValue": "PEUGEOT"
+                },
+                "ModelDescription": {
+                "CurrentTextValue": "307 X-LINE"
+                },
+                "EngineSize": {
+                "CurrentTextValue": "1360"
+                },
+                "BodyStyle": {
+                "CurrentTextValue": "Motorbike"
+                },
+                "FuelType": {
+                "CurrentTextValue": "PETROL"
+                },
+                "Variant": "",
+                "Colour": "SILVER",
+                "VehicleIdentificationNumber": "VF33CKFUC84922414",
+                "KType": "",
+                "EngineNumber": "FE040407358",
+                "ImageUrl": "https://www.regcheck.org.uk/image.aspx/@UEVVR0VPVCAzMDcgWC1MSU5FfG1vdG9yY3ljbGU="
+                }
